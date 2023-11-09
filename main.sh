852b97a494ddc060fdb8031c40438eb405f70ec5
teramotodaiki <i101323@gmail.com>
2023-11-09 13:29:04 +0900

    セカンドディスプレイ

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 imgPath="$HOME/github/big-brother-is-watching-you/img/$(date +%Y%m%d%H%M%S).png"
 
 # スクリーンショットを撮影
-screencapture -x $imgPath
+screencapture -x -D 2 $imgPath
 
 # Node.jsのスクリプトを実行
 node ~/github/big-brother-is-watching-you/index.js $imgPath
